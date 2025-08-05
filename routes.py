77151e9a566b539fc79a4a8c55e2f479406fcd5f
Sudesh Subedi <nooneandnothing09@gmail.com>
2025-08-05 22:26:06 +0545

    Finally, A PROPER CRUD OPERATION

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 
 from models import Person
 
@@ -6,46 +6,60 @@ from app import db
 
 def register_routes(app,db):
     @app.route('/', methods = ['POST', 'GET'])
-
     def index():
         if request.method == "GET":
             print("DB path:", db.engine.url)
             people = Person.query.all()
-
-            return render_template('index.html', people = people)
+            return render_template('index.html', people=people)
         
         elif request.method == "POST":
             name = request.form.get('name')
             age = int(request.form.get('age'))
 
-            person = Person(name=name, age=age,)
-
+            person = Person(name=name, age=age)
             db.session.add(person)
             db.session.commit()
 
-            people = Person.query.all()
-            return render_template('index.html', people = people)
+            # Redirecting to GET after POST to prevent duplicate on reload
+            return redirect(url_for('index'))
         
-    @app.route('/clear_all')
+    # @app.route('/clear_all')
 
-    def clear_all():
-        db.session.query(Person).delete()
-        db.session.commit()
-        return render_template('index.html')
+    # def clear_all():
+    #     db.session.query(Person).delete()
+    #     db.session.commit()
+    #     return render_template('index.html')
     
 
-    @app.route('/delete/<int:pid>', methods=['DELETE'])
+    @app.route('/delete/<int:pid>', methods=['POST'])
     def delete_pid(pid):
-        print(f"Trying to delete person with pid={pid}")
+        print(f"Deleting person with pid={pid}")
         Person.query.filter(Person.pid == pid).delete()
         db.session.commit()
-        return '', 204
+        return redirect(url_for('index'))
+
     
     @app.route('/detail/<int:pid>')
     def detail(pid):
         person = Person.query.filter(Person.pid==pid).first()
 
         return render_template('detail.html',person=person)
+    
+
+    @app.route('/update_things/<int:pid>', methods = ['GET', 'POST'])
+    def update_things(pid):
+        person = Person.query.get(pid)
+        if request.method == 'POST':
+            person.name = request.form.get('name')
+            person.age = request.form.get('age')
+
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
+
+        return render_template('update_things.html', person=person)        
+
         
 
         
